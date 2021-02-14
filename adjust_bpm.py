9908f061d04c39df467ed04dfb44092e50c7e375
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-02-14 13:49:50 -0800

    update

---
+++ adjust_bpm.py
@@ -0,0 +1,14 @@
+# produce a transformed set of wave files, with consistent tempo
+import os
+import sys
+
+wavs = [x.strip() for x in os.popen("ls -1 *.wav").readlines()]
+
+def run(c):
+	print(c)
+	a = os.system(c)
+
+run("mkdir out")
+
+for w in wavs:
+	run("soundstretch " + '"' + w + '" out/"' + w + '" -bpm=127')
