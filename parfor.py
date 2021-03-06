37c24c41284d1b13e452f2b64e70bf4ed4d7287a
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-06 15:32:30 -0800

    update

--- parfor.py
+++ parfor.py
@@ -4,3 +4,6 @@ def parfor(my_function, my_inputs):
     pool = mp.Pool(mp.cpu_count())
     result = pool.map(my_function, my_inputs)
     return(result)
+
+    # for i, _ in enumerate(p.imap_unordered(do_work, xrange(num_tasks)), 1):
+    #    sys.stderr.write('\rdone {0:%}'.format(i/num_tasks))
