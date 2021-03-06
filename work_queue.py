59474b8f0ac2092d8459cf91392f018686c5c3df
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-06 14:21:46 -0800

    update

--- work_queue.py
+++ work_queue.py
@@ -11,55 +11,55 @@ def err(m):
     print("Error: " + m)
     sys.exit(1)
 
-class work_queue:
-    self.jobs = []
+      
 
+
+class work_queue:
     def __init__(self):
         self.jobs = []
 
     def add(self, job):
         self.jobs.append(job)
 
-    def run(self, ncpu = multiprocessing.cpu_count()):
+    # printf with lock
+    def cprint(self, s):
+        self.p_lock.acquire()
+        print(s)
+        self.p_lock.release()
 
-        lock, p_lock = allocate_lock(), allocate_lock() # lock mechanism
-        threads_alive, next_j, j_max = ncpu, 0, n_task - 1
+    def run(self, ncpu = multiprocessing.cpu_count()):
+        n_task = len(self.jobs)
+        self.lock, self.p_lock = allocate_lock(), allocate_lock() # lock mechanism
+        self.threads_alive, self.next_j, self.j_max = ncpu, 0, n_task - 1
     
-        # printf with lock
-        def cprint(s):
-            global p_lock
-            p_lock.acquire()
-            print(s)
-            p_lock.release()
-    
-        cprint("nworkers " + str(ncpu))
+        self.cprint("nworkers " + str(ncpu))
     
         def threadfun(my_id):  # worker thread picks up task
-            global next_j, j_max, lock, threads_alive, jobs
+            # global next_j, j_max, lock, threads_alive, jobs
             job_times = [] # fill this in later
             while True:
-                lock.acquire()
-                j, next_j = next_j, next_j + 1  # pick up task idx
-                lock.release()
+                self.lock.acquire()
+                j, self.next_j = self.next_j, self.next_j + 1  # pick up task idx
+                self.lock.release()
     
-                if(j > j_max):
-                    threads_alive -= 1  # kill thread if no work
+                if(j > self.j_max):
+                    self.threads_alive -= 1  # kill thread if no work
                     return
     
-                if jobs[j].strip() == "":  # don't run empty task
+                if self.jobs[j].strip() == "":  # don't run empty task
                     continue
     
-                work = jobs[j].split(";")  # divide task into subtasks?
+                work = self.jobs[j].split(";")  # divide task into subtasks?
                 for i in range(0, len(work)):
-                    cprint("worker(" + str(my_id) + "): " + work[i])
+                    self.cprint("worker(" + str(my_id) + "): " + work[i])
                     os.popen(work[i]).read()
-                    cprint("\tworker(" + str(my_id) + ")")
+                    self.cprint("\tworker(" + str(my_id) + ")")
     
         def wait_to_finish():  # sleep a bit?
             poll_int = .01 # polling is bad, don't do too much
             while True:
                 time.sleep(poll_int)
-                if(threads_alive == 0):
+                if(self.threads_alive == 0):
                     sys.exit(0)
     
         for i in range(0, ncpu):
@@ -73,15 +73,17 @@ if __name__ == "__main__":
     args = sys.argv
 
     if len(args) < 2:
-        err('multicore.py:\nusage: multicore [text file: one sys cmd per line] # init one thread per cpu\nmulticore [text file: one syscmd per line] 1 # init one thread per job')
+        err('multicore.py:\nusage:\n\tmulticore [text file: one sys cmd per line] # init one thread per cpu' +
+            '\n\tmulticore [text file: one syscmd per line] 1 # init one thread per job' + 
+            '\n\tmulticore [text file: one syscmd per line] 4 # init 4 threads')
 
-    one_worker_per_job, n_workers = False, None
+    one_worker_per_job, n_workers, fn = False, None, args[1]
 
     if len(args) > 2:
-        one_worker_per_job = True if sys.argv[2] == "1" else False
+        one_worker_per_job = True if args[2] == "1" else False
         try:
-            if int(sys.argv[2]) > 1:
-                n_workers = int(sys.argv[2])  # workers specified
+            if int(args[2]) > 1:
+                n_workers = int(args[2])  # workers specified
         except:
             pass
 
